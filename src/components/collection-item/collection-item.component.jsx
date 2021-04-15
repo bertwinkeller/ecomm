e692c37d78d410eee56743708d7b0f8f1616edd7
Bert Keller <bertwinkeller@gmail.com>
2021-04-15 15:23:57 -0700

    collection items

---
+++ src/components/collection-item/collection-item.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+ const CollectionItem = ({id, name, price, imageUrl}) => {
+    return (
+        <div className='collection-item'>
+            <div className='image'
+            
+        </div>
+    )
+}
+
+
+export default CollectionItem
